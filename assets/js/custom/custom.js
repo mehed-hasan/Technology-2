106fc1d8bbcd1855639fa9b4c341e815c224f101
mehed-hasan <mehedi.hasan@intelli.global>
2022-04-18 19:02:58 +0600

    Upload portfolio details page

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -1,13 +1,13 @@
 
 
 // Code for preloader will be disappeared after all contents being loaded ----------------------------------------------------------*****
-// $(window).on('load', function () {
-//     $(".left_bar ").fadeOut(500); 
-//     $(".left_bar img").fadeOut(200);
-//     $("#main").css({'overflow':'unset'}); 
+$(window).on('load', function () {
+    // $(".left_bar ").fadeOut(500); 
+    // $(".left_bar img").fadeOut(200);
+    // $("#main").css({'overflow':'unset'}); 
 
 
-// });
+});
 // Code for preloader will be disappeared after all contents being loaded ######################################-------------------------- Ended 
 
 
