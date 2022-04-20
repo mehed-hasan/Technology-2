6c4ee54836007a6e2183073cd3d85b3e42bbfab4
mehed-hasan <mehedi.hasan@intelli.global>
2022-04-20 17:34:13 +0600

    Full project v1

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -2,9 +2,9 @@
 
 // Code for preloader will be disappeared after all contents being loaded ----------------------------------------------------------*****
 $(window).on('load', function () {
-    // $(".left_bar ").fadeOut(500); 
-    // $(".left_bar img").fadeOut(200);
-    // $("#main").css({'overflow':'unset'}); 
+    $(".left_bar ").fadeOut(500); 
+    $(".left_bar img").fadeOut(200);
+    $("#main").css({'overflow':'unset'}); 
 
 
 });
