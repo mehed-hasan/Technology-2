10d2de97c873c0057e75fa922985a70a2e3f1045
mehed-hasan <mehedi.hasan@intelli.global>
2022-08-11 15:05:34 +0600

    Test

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -1,7 +1,7 @@
 
 
 // Code for preloader will be disappeared after all contents being loaded ----------------------------------------------------------*****
-$(window).on('load', function () {
+$(document).on('load', function () {
     $(".left_bar ").fadeOut(500); 
     $(".left_bar img").fadeOut(200);
     $("#main").css({'overflow':'unset'}); 
